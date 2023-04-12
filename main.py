6e7e4ce688448df926dce2d503400a973646e1ff
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2023-04-13 00:30:44 +0530

    df

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ arr = np.array([3, 4, 5])
 
 # print(np.zeros((4,5), dtype='int32')) // np.ones // np.empty
 
-arr2 = arr[1 : 3].copy()
-arr2[0] = 9
-print(arr)
+# SLICING '''
+# arr2 = arr[1 : 3].copy()
+# arr2[0] = 9
+# print(arr)
