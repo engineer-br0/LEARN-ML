225bebea60dbbe26691db037175125e843fe906a
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2023-04-13 15:36:28 +0530

    matplotlib over

---
+++ matplot.py
@@ -0,0 +1,8 @@
+# from matplotlib import pyplot as plt
+
+# plt.plot([1,2,3], [4,7,9], label='first hai')
+# plt.title("mridul bhai ka plot")
+# plt.xlabel("x axe")
+# plt.plot([6,6,7], [9,66,65], label='second')
+# plt.legend()
+# plt.show()
